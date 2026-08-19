0221e83ecae1e1d214990af951e8df819ad2d318
baileyb2026 <bethanie01@gmail.com>
2026-08-19 21:31:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,33 @@
 const CACHE="solitaire-v2";
 const ASSETS=["./","./index.html","./app.js","./manifest.json","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let c=x.clone();caches.open(CACHE).then(c=>c.put(e.request,c));return x}).catch(()=>caches.match("./index.html")))));
+
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate",e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>
+      Promise.all(
+        keys.filter(k=>k.startsWith("solitaire-") && k!==CACHE)
+            .map(k=>caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>
+      r||fetch(e.request).then(x=>{
+        const c=x.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,c));
+        return x;
+      }).catch(()=>caches.match("./index.html"))
+    )
+  );
+});
