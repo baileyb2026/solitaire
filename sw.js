3a1129051ff6f2fdef8fcec7960ed4b61ba06169
baileyb2026 <bethanie01@gmail.com>
2026-08-19 21:31:12 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="solitaire-v1";
+const CACHE="solitaire-v2";
 const ASSETS=["./","./index.html","./app.js","./manifest.json","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
